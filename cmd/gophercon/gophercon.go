cab60ac9ae0848aba0ec35999f43ef6c461625df
Hinrik Gylfason <hinrikg@gmail.com>
2018-05-31 10:16:16 +0000

    refactor handler to a factory function

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -7,9 +7,13 @@ import "log"
 func main() {
 	log.Printf("Service starting ...")
 
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/home", homeHandler())
+	http.ListenAndServe(":8000", nil)
+}
+
+func homeHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Request is processing %s", r.URL.Path)
 		fmt.Fprint(w, "Hello!")
-	})
-	http.ListenAndServe(":8000", nil)
+	}
 }
