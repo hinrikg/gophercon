3633611f8da6cc4e1214d8c8f0a52b4d1575c66d
Hinrik Gylfason <hinrikg@gmail.com>
2018-05-31 11:11:28 +0000

    add error logging

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -8,5 +8,5 @@ import "github.com/hinrikg/gophercon/pkg/routing"
 func main() {
 	log.Printf("Service starting ...")
 	router := routing.CreateRouter()
-	http.ListenAndServe(":8000", router)
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
