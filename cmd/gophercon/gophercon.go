63f2c3c36f1647127ea9ae4a8bf1c4f705294314
Hinrik Gylfason <hinrikg@gmail.com>
2018-05-31 11:16:02 +0000

    add port configuration via env variable

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -1,12 +1,19 @@
 package main
 
-import "net/http"
 import "log"
+import "net/http"
+import "os"
 
 import "github.com/hinrikg/gophercon/pkg/routing"
 
 func main() {
 	log.Printf("Service starting ...")
+
+	port := os.Getenv("SERVICE_PORT")
+	if len(port) == 0 {
+		log.Fatal("Service port was not set!")
+	}
+
 	router := routing.CreateRouter()
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
